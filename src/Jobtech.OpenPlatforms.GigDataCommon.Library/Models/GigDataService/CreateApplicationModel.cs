c364b31f1ce79cf6e169b7e2977ab8f80d393b24
Björn Milton <bjorn@roombler.com>
2020-01-27 14:38:02 +0100

    Added new classes that models the interaction on admin level between gig-data-api and gig-platform-api

---
+++ src/Jobtech.OpenPlatforms.GigDataCommon.Library/Models/GigDataService/CreateApplicationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Jobtech.OpenPlatforms.GigDataCommon.Library.Models.GigDataService
+{
+    public class CreateApplicationModel
+
+    {
+        public string Name { get; set; }
+        public string NotificationEndpointUrl { get; set; }
+        public string EmailVerificationNotificationEndpointUrl { get; set; }
+        public string AuthCallbackUrl { get; set; }
+    }
+}
---
+++ src/Jobtech.OpenPlatforms.GigDataCommon.Library/Models/GigDataService/CreateApplicationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Jobtech.OpenPlatforms.GigDataCommon.Library.Models.GigDataService
+{
+    public class CreateApplicationResult
+    {
+        public string SecretKey { get; set; }
+        public string ApplicationId { get; set; }
+    }
+}
