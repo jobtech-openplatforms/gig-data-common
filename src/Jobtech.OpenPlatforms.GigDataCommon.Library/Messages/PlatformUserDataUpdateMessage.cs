c70fed4d2e32ef2fc8845617d868f0dbe2c78af4
Björn Milton <bjorn@roombler.com>
2020-05-07 15:33:06 +0200

    Added PlatformDataUpdateResultType so that we can communicate various types of outcomes to the consumer of the message.

--- src/Jobtech.OpenPlatforms.GigDataCommon.Library/Messages/PlatformUserDataUpdateMessage.cs
+++ src/Jobtech.OpenPlatforms.GigDataCommon.Library/Messages/PlatformUserDataUpdateMessage.cs
@@ -5,17 +5,27 @@ namespace Jobtech.OpenPlatforms.GigDataCommon.Library.Messages
 {
     public class PlatformUserUpdateDataMessage
     {
-        public PlatformUserUpdateDataMessage(string requestId, string userName, Guid platformId, PlatformDataUserUpdateResult platformData)
+        public PlatformUserUpdateDataMessage(string requestId, string userName, Guid platformId, PlatformDataUserUpdateResult platformData, PlatformDataUpdateResultType resultType = PlatformDataUpdateResultType.Succeess)
         {
             RequestId = requestId;
             UserName = userName;
             PlatformId = platformId;
             PlatformData = platformData;
+            ResultType = resultType;
         }
 
         public string RequestId { get; private set; }
         public string UserName { get; private set; }
         public Guid PlatformId { get; private set; }
         public PlatformDataUserUpdateResult PlatformData { get; private set; }
+        public PlatformDataUpdateResultType ResultType { get; private set; }
+    }
+
+    public enum PlatformDataUpdateResultType
+    {
+        Succeess,
+        UserNotFound,
+        MalformedDataResponse,
+        PlatformCommunicationError
     }
 }
